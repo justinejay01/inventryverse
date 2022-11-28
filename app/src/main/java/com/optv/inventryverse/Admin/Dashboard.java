56c8c07b4dbeba7c57a4fd7156033ef00f444c85
OLIVAR-PC <justinej.olivar@gmail.com>
2022-11-28 10:38:03 +0800

    Add All files v1-a3

--- app/src/main/java/com/optv/inventryverse/Admin/Dashboard.java
+++ app/src/main/java/com/optv/inventryverse/Admin/Dashboard.java
@@ -1,5 +1,6 @@
 package com.optv.inventryverse.Admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
@@ -7,6 +8,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.optv.inventryverse.R;
 
@@ -16,6 +19,26 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dashboard_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.adminSettings:
+                Intent i = new Intent();
+                i.setClass(getApplicationContext(), Settings.class);
+                startActivity(i);
+                break;
+            default:
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
